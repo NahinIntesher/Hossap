c80e26c8bf6da318ff69ed41dc4500f412ce0592
Assassin318554 <nahinsikdar123@gmail.com>
2024-11-14 08:59:00 +0600

    modify

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,10 +3,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
 import "../global.css"; // Ensure your Tailwind setup works correctly here
 
-export default function Layout() {
+const MainLayout = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <Slot />
     </SafeAreaView>
   );
+};
+
+export default function Layout() {
+  return <MainLayout />;
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
+import { ActivityIndicator } from "react-native";
 
 export default function Index() {
   return (
     <View className="bg-sky-300 p-20">
       <Text className="text-3xl text-center">Home</Text>
     </View>
+    // <View className="flex-1 justify-center items-center">
+    //   <ActivityIndicator size="large" color="#4e4f4d" />
+    // </View>
   );
 }
