06c5965c9bfffda66f9d4c8ebb12a0ec76d7c59e
nurulalamador <nurulalomador@gmail.com>
2024-11-14 16:58:43 +0600

    a

--- app/index.jsx
+++ app/index.jsx
@@ -6,6 +6,7 @@ import { useRouter } from "expo-router";
 
 export default function Onboarding() {
   const [isFirstTime, setIsFirstTime] = useState(true);
+  const [isLoaded, setLoaded] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -15,6 +16,9 @@ export default function Onboarding() {
       if (hasSeenOnboarding) {
         setIsFirstTime(false);
         router.push("/logIn");
+      }
+      else {
+        setLoaded(true);
       }
     };
     checkFirstTime();
@@ -27,35 +31,38 @@ export default function Onboarding() {
     router.push("/signUp");
   };
 
-  return (
-    <View className="flex-1 p-8 justify-center items-center ">
-      <Image
-        source={require("../assets/images/welcomeImage.png")}
-        style={{ width: 250, height: 250 }}
-        className="rounded-3xl border-gray-100 mb-5"
-        resizeMode="contain"
-      />
-      <Text className="text-4xl text-black font-bold text-center mb-4">
-        Welcome to Hossap
-      </Text>
-      <Text className="text-lg text-black text-center mb-8">
-        Stay connected with friends and family, chat anytime, anywhere. Let's
-        get started!
-      </Text>
+  if(isLoaded) {
+    return (
 
-      <TouchableOpacity
-        className="bg-[#6c63ff] py-4 px-8 rounded-full"
-        onPress={handleContinue}
-      >
-        <Text className="text-white text-lg font-bold">Get Started</Text>
-      </TouchableOpacity>
-      <Link href="/logIn" className="mt-4">
-        <Text className="text-md">Already have an account? </Text>
-
-        <Text style={{ color: "#6c63ff" }} className="text-md font-bold ">
-          Login
+      <View className="flex-1 p-8 justify-center items-center ">
+        <Image
+          source={require("../assets/images/welcomeImage.png")}
+          style={{ width: 250, height: 250 }}
+          className="rounded-3xl border-gray-100 mb-5"
+          resizeMode="contain"
+        />
+        <Text className="text-4xl text-black font-bold text-center mb-4">
+          Welcome to Hossap
         </Text>
-      </Link>
-    </View>
-  );
+        <Text className="text-lg text-black text-center mb-8">
+          Stay connected with friends and family, chat anytime, anywhere. Let's
+          get started!
+        </Text>
+  
+        <TouchableOpacity
+          className="bg-[#6c63ff] py-4 px-8 rounded-full"
+          onPress={handleContinue}
+        >
+          <Text className="text-white text-lg font-bold">Get Started</Text>
+        </TouchableOpacity>
+        <Link href="/logIn" className="mt-4">
+          <Text className="text-md">Already have an account? </Text>
+  
+          <Text style={{ color: "#6c63ff" }} className="text-md font-bold ">
+            Login
+          </Text>
+        </Link>
+      </View>
+    );
+  } 
 }
