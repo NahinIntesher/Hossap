7152437af1f75258d4c26059ab0b9dedbf8eff31
nurulalamador <nurulalomador@gmail.com>
2024-11-14 09:53:00 +0600

    app home update

--- app/(app)/home.jsx
+++ app/(app)/home.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <View style={styles.topContainer}>
+        <View style={{ flex: 2, backgroundColor: "red" }}>
+          <Text>Nahin</Text>
+          <Ionicons name="search" size={32} color="green" />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text>Hello</Text>
+          <Text>Nahin</Text>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  topContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
